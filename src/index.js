883556b30a7d5681b3e7afadd732797ff2fada41
Surya <68083083+suryasuren@users.noreply.github.com>
2024-02-25 03:01:21 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,22 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "./store/store";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhfZttUPnfgYbtDFXh2_BGQ0uh96QIEoU",
+  authDomain: "calibraint-app.firebaseapp.com",
+  projectId: "calibraint-app",
+  storageBucket: "calibraint-app.appspot.com",
+  messagingSenderId: "677473451219",
+  appId: "1:677473451219:web:d8c8f9f3e581297aa22826",
+  measurementId: "G-SDDBW6E0TJ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
